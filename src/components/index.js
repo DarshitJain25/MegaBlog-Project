4468be20118cd87600acad2866656269ae317f2d
DarshitJain25 <darshitjain25oct@gmail.com>
2026-08-09 02:50:57 +0530

    Fix issue of SelectButton component ; name changed to tempSelect

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import SelectButton from "./SelectButton";
+import SelectButton from "./tempSelect";
 import Button from "./button";
 import Input from "./input";
 import RTE from "./RTE";

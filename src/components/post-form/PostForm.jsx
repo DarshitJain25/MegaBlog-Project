cd7ccf827fa2f49001965bab6f016e897f94aba1
DarshitJain25 <darshitjain25oct@gmail.com>
2026-08-09 02:20:55 +0530

    Import errors fixed

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,9 @@
 import React, { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Input, SelectButton, RTE } from "../index";
+import Button from "../Button";
+import Input from "../Input";
+import RTE from "../RTE";
+import SelectButton from "../SelectButton";
 import { useDispatch } from "react-redux";
 import dbService from "../../appwrite/conf";
 import { useNavigate } from "react-router-dom";
